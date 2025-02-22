9622e8235d34d954485a8d429e1025b0908ea7dc
asliwxhm <59582568+asliwxhm@users.noreply.github.com>
2025-02-22 08:58:34 -0500

    Create clink.lua

---
+++ Tools/cmder/clink.lua
@@ -0,0 +1,19 @@
+-- 
+--[[
+> hostname
+DESKTOP-5NVTL9O
+
+> echo %CMDER_CONFIG_DIR%
+C:\Tools\cmder\config
+
+c:\Tools\cmder\vendor\clink\clink.lua
+c:\Tools\cmder\vendor\clink\clink.lua
+
+> net use x: \\W10X64-LT-01\CDrive /persistent:ye
+Y:\toolsbeta\cmder\vendor\clink\clink.lua
+]]
+-- This file is intentionally blank.
+--
+-- The old clink.lua file is no longer used, and this blank file ensures if a
+-- new clink version is copied over an old clink version, it won't accidentally
+-- use an obsolete leftover clink.lua file.
